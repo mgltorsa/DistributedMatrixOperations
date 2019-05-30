7d4a3a73e51ba6ef3463f3b658543835405ee36f
mgltorsa <mgltorsa@gmail.com>
2019-05-30 17:36:11 -0500

    updated composites

--- operations/src/main/java/co/edu/icesi/MatrixOperationsRegister.java
+++ operations/src/main/java/co/edu/icesi/MatrixOperationsRegister.java
@@ -1,12 +1,11 @@
 package co.edu.icesi;
 
-import java.rmi.Naming;
+import java.net.InetAddress;
 
 import org.osoa.sca.annotations.Property;
 import org.osoa.sca.annotations.Reference;
 import org.osoa.sca.annotations.Service;
 
-import co.edu.icesi.configurations.IBrokerConfiguration;
 import co.edu.icesi.interfaces.IBroker;
 
 /**
@@ -15,38 +14,27 @@ import co.edu.icesi.interfaces.IBroker;
 @Service(interfaces = Runnable.class)
 public class MatrixOperationsRegister implements Runnable {
 
-	@Reference
-	private IBrokerConfiguration configuration;
-	
 	private IBroker broker;
 	
 	@Property
 	private int port;
 	
 	@Property
-	private String serviceName;
+	private String serviceName;	
 	
 	
-
+	@Reference(name="broker")
+	public void setBroker(IBroker broker) {
+		this.broker=broker;
+	}
 	
 
 	private void registerAsService() throws Exception {
 		
-		setBrokerStub();
-		// String ip = InetAddress.getLocalHost().getHostAddress();
-		String ip = "localhost";
+		String ip = InetAddress.getLocalHost().getHostAddress();
 		System.out.println("Myip->"+ip);
 		System.out.println("service name->"+serviceName);
 		broker.register("rmi", ip, port, serviceName);
-	}
-
-	private void setBrokerStub() throws Exception {
-		String brokerHost = configuration.getBrokerHost();
-		int brokerPort = configuration.getBrokerPort();
-		String brokerService = configuration.getBrokerService();
-		
-		System.out.println("rmi://"+brokerHost+":"+brokerPort+"/"+brokerService);
-		broker = (IBroker) Naming.lookup("rmi://"+brokerHost+":"+brokerPort+"/"+brokerService);
 	}
 	
 	@Override
