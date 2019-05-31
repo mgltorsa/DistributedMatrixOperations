1df616ede221997703cbbea5f6a62ef381b44676
mgltorsa <mgltorsa@gmail.com>
2019-05-31 15:21:37 -0500

    removed Remote extensions in Broker

--- netservices/src/main/java/co/edu/icesi/interfaces/IBroker.java
+++ netservices/src/main/java/co/edu/icesi/interfaces/IBroker.java
@@ -1,16 +1,14 @@
 package co.edu.icesi.interfaces;
 
-import java.rmi.Remote;
-import java.rmi.RemoteException;
 import java.util.List;
 
 import org.osoa.sca.annotations.Service;
 
 @Service
-public interface IBroker extends Remote {
+public interface IBroker{
 
-	public void register(String protocol, String ip, int port, String service) throws IllegalArgumentException, RemoteException;
+	public void register(String protocol, String ip, int port, String service) throws IllegalArgumentException;
 
-	public List<String> getServicesIP(String service) throws IllegalArgumentException, RemoteException;
+	public List<String> getServicesIP(String service) throws IllegalArgumentException;
 
 }
