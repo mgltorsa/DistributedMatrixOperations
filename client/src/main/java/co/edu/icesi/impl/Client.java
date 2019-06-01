d4755263fc315e725c44e61eecea6eef0e37914e
mgltorsa <mgltorsa@gmail.com>
2019-06-01 18:44:00 -0500

    updated architecture

--- client/src/main/java/co/edu/icesi/impl/Client.java
+++ client/src/main/java/co/edu/icesi/impl/Client.java
@@ -1,19 +1,29 @@
 package co.edu.icesi.impl;
 
+import java.awt.image.BufferedImage;
+import java.awt.image.WritableRaster;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.imageio.ImageReader;
+import javax.imageio.stream.ImageInputStream;
+
 import org.osoa.sca.annotations.Reference;
 import org.osoa.sca.annotations.Service;
 
 import co.edu.icesi.interfaces.IServer;
 
-@Service(interfaces=Runnable.class)
-public class Client implements Runnable{
-	
+@Service(interfaces = Runnable.class)
+public class Client implements Runnable {
+
 	private IServer server;
 
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
 		server.recieve("./data/source/img.jpg", "./data/dest/img.jpg", 45d);
+
 	}
 
 	@Reference(name="server")
