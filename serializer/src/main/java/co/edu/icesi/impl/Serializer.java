c837e9d9ed2de85e9078e9e258b6bd34da332826
mgltorsa <mgltorsa@gmail.com>
2019-06-03 15:19:32 -0500

    ISerialixer fixed

---
+++ serializer/src/main/java/co/edu/icesi/impl/Serializer.java
@@ -0,0 +1,11 @@
+package main.java.co.edu.icesi.impl;
+
+import main.java.co.edu.icesi.interfaces.ISerializer;
+
+/**
+ * Serializer
+ */
+public class Serializer implements ISerializer{
+
+    
+}
--- serializer/src/main/java/co/edu/icesi/interfaces/ISerializer.java
+++ serializer/src/main/java/co/edu/icesi/interfaces/ISerializer.java
@@ -1,15 +1,17 @@
-package co.edu.icesi.interfaces;
+package main.java.co.edu.icesi.interfaces;
 
 /**
  * ISerializer
  */
 public interface ISerializer {
 
-	public void setWorkers(int workers);
+    public void setWorkers(int workers);
 	
-	public void setDestPath(String path);
+    public void setDestPath(String path);
+    
+    public void setSourcePath(String path);
 	
 	public boolean isLocked();
 	
-	public boolean drawImage(int x, int y, int width, int height, int[][] image);
+    public boolean drawImage(int x, int y, int width, int height, int[][] image);
 }
